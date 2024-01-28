af851acf518d77b897a933be6d9a63619fbdb47d
Deepak <deep84chaudhary@gmail.com>
2024-01-28 21:46:55 +0530

    modify the footer component UI

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,61 @@
-import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-    const year = new Date().getFullYear();
-    return (
-        <footer className="bg-gray-700 text-white h-[10vh] flex items-center justify-center text-lg">
-            <p>© All rights reserved || {year}</p>
-        </footer>
-    );
-}
+
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="bg-[#1b2124] min-h-[10vh] text-gray-400 p-4">
+
+        <div className="flex items-center justify-center xl:items-baseline xl:flex-row flex-col sm:gap-10 gap-5">
+
+            <div className="flex items-center justify-center mr-10">
+                <Image width={250} height={250} src="/logo.png" alt="pw-logo" />
+            </div>
+
+            <div className="flex flex-col gap-5 pb-5">
+                <div className="flex flex-col gap-3">
+                    <h1 className="text-[#ffffff] font-semibold text-xl">Contact</h1>
+                    <div className="border-[#0000ff] border-[2.4px] rounded-lg w-[16rem]"></div>
+                </div>
+                <div className="flex flex-col gap-5">
+                    <p>support@instablogger.com</p>
+                    <p>+91-7666122288</p>
+                </div>
+            </div>
+
+            <div className="flex flex-col gap-5">
+                <div className="flex flex-col gap-3">
+                    <h1 className="text-[#ffffff] font-semibold text-xl">Company</h1>
+                    <div className="border-[#0000ff] border-[2.4px] rounded-lg w-[16rem]"></div>
+                </div>
+                <div className="flex justify-between">
+                    <ul className="flex flex-col gap-2 mb-2">
+                        <li>
+                            <Link href="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link href="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                    <ul className="flex flex-col gap-2">
+                        <li>
+                            <Link href="/">Terms and condition</Link>
+                        </li>
+                        <li>
+                            <Link href="/">Privacy policy</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="text-center text-white">
+                InstaBlogger || © All rights reserved || {year}
+            </div>
+        </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -18,55 +18,55 @@ const LandingPage = () => {
 
     </section>
     <section className="flex flex-col gap-8 mt-8">
-        <div class=" flex flex-col items-center sm:items-start text-blue-600">
-            <h1 class="sm:text-4xl text-2xl font-bold">Explore Amazing Features</h1>
-            <p class="sm:text-lg text-base text-[#dddcdc] font-thin text-center">
+        <div className=" flex flex-col items-center sm:items-start text-blue-600">
+            <h1 className="sm:text-4xl text-2xl font-bold">Explore Amazing Features</h1>
+            <p className="sm:text-lg text-base text-[#dddcdc] font-thin text-center">
             A one-stop destination for social media activity or blogging...
             </p>
         </div>
-        <div class="flex items-center justify-center gap-8 flex-wrap">
-            <div class="flex flex-col gap-8">
+        <div className="flex items-center justify-center gap-8 flex-wrap">
+            <div className="flex flex-col gap-8">
                 
-                <div class="md:w-[460px] xl:w-[250px] 2xl:w-[350px] p-4 rounded-lg bg-[#c8f5d2] hover:bg-[#bffccd] shadow-[0_5px_10px_#0000ff] hover:shadow-[0_10px_10px_#0000ff]">
+                <div className="md:w-[460px] xl:w-[250px] 2xl:w-[350px] p-4 rounded-lg bg-[#c8f5d2] hover:bg-[#bffccd] shadow-[0_5px_10px_#0000ff] hover:shadow-[0_10px_10px_#0000ff]">
                     <Image width={52} height={52} src="/products-icon1.png" alt="products-icon1" />
-                    <h1 class="font-bold text-xl my-4">InstaBlogger Creation</h1>
-                    <h2 class="text-lg font-extralight text-[#000]">Elevate your voice share your thoughts, stories, and ideas effortlessly. </h2>
+                    <h1 className="font-bold text-xl my-4">InstaBlogger Creation</h1>
+                    <h2 className="text-lg font-extralight text-[#000]">Elevate your voice share your thoughts, stories, and ideas effortlessly. </h2>
                 </div>
 
-                <div class="md:w-[460px] xl:w-[250px] 2xl:w-[350px] p-4 rounded-lg bg-[#f7bdef] hover:bg-[#f8a3ed] shadow-[0_5px_10px_#0000ff] hover:shadow-[0_10px_10px_#0000ff]"
+                <div className="md:w-[460px] xl:w-[250px] 2xl:w-[350px] p-4 rounded-lg bg-[#f7bdef] hover:bg-[#f8a3ed] shadow-[0_5px_10px_#0000ff] hover:shadow-[0_10px_10px_#0000ff]"
                     >
                     <Image width={52} height={52} src="/products-icon2.png" alt="products-icon1" />
-                    <h1 class="font-bold text-xl py-4">InstaBlogger Updation</h1>
-                    <h2 class="text-lg font-extralight text-[#000]">Experience the freedom to seamlessly edit your posts with updation feature.</h2>
+                    <h1 className="font-bold text-xl py-4">InstaBlogger Updation</h1>
+                    <h2 className="text-lg font-extralight text-[#000]">Experience the freedom to seamlessly edit your posts with updation feature.</h2>
                 </div>
                 
-                <div class="md:w-[460px] xl:w-[250px] 2xl:w-[350px] p-4 rounded-lg bg-[#f79393] hover:bg-[#fd7777] shadow-[0_5px_10px_#0000ff] hover:shadow-[0_10px_10px_#0000ff]"
+                <div className="md:w-[460px] xl:w-[250px] 2xl:w-[350px] p-4 rounded-lg bg-[#f79393] hover:bg-[#fd7777] shadow-[0_5px_10px_#0000ff] hover:shadow-[0_10px_10px_#0000ff]"
                     >
                     <Image width={52} height={52} src="/products-icon3.png" alt="products-icon1" />
-                    <h1 class="font-bold text-xl py-4">InstaBlogger Deletion</h1>
-                    <h2 class="text-lg font-extralight text-[#000]">Experience the freedom to seamlessly delete your posts with deletion feature.</h2>
+                    <h1 className="font-bold text-xl py-4">InstaBlogger Deletion</h1>
+                    <h2 className="text-lg font-extralight text-[#000]">Experience the freedom to seamlessly delete your posts with deletion feature.</h2>
                 </div>
             </div>
 
-            <div class="flex flex-col gap-8">
+            <div className="flex flex-col gap-8">
                 
-                <div class="md:w-[460px] xl:w-[250px] 2xl:w-[350px] p-4 rounded-lg bg-[#f3f3ba] hover:bg-[#fbfba7] shadow-[0_5px_10px_#0000ff] hover:shadow-[0_10px_10px_#0000ff]"
+                <div className="md:w-[460px] xl:w-[250px] 2xl:w-[350px] p-4 rounded-lg bg-[#f3f3ba] hover:bg-[#fbfba7] shadow-[0_5px_10px_#0000ff] hover:shadow-[0_10px_10px_#0000ff]"
                     >
 
                     <Image width={52} height={52} src="/products-icon4.png" alt="products-icon1" />
-                    <h1 class="font-bold text-xl py-4">InstaBlogger Like & Unlike </h1>
-                    <h2 class="text-lg font-extralight text-[#000]">Express your appreciation by liking the blogs or also unlike anytime.</h2>
+                    <h1 className="font-bold text-xl py-4">InstaBlogger Like & Unlike </h1>
+                    <h2 className="text-lg font-extralight text-[#000]">Express your appreciation by liking the blogs or also unlike anytime.</h2>
                 </div>
                 
-                <div class="md:w-[460px] xl:w-[250px] 2xl:w-[350px] p-4 rounded-lg bg-[#88ddde] hover:bg-[#5af5f2] shadow-[0_5px_10px_#0000ff] hover:shadow-[0_10px_10px_#0000ff]"
+                <div className="md:w-[460px] xl:w-[250px] 2xl:w-[350px] p-4 rounded-lg bg-[#88ddde] hover:bg-[#5af5f2] shadow-[0_5px_10px_#0000ff] hover:shadow-[0_10px_10px_#0000ff]"
                 >
                     <Image width={52} height={52} src="/products-icon5.png" alt="products-icon1" />
-                    <h1 class="font-bold text-xl py-4">InstaBlogger Comment</h1>
-                    <h2 class="text-lg font-extralight text-[#000]">Express your views on blogs by commenting your thoughts.</h2>
+                    <h1 className="font-bold text-xl py-4">InstaBlogger Comment</h1>
+                    <h2 className="text-lg font-extralight text-[#000]">Express your views on blogs by commenting your thoughts.</h2>
                 </div>
             </div>
 
-            <div class="max-w-[550px] shadow-[0_5px_10px_#0000ff] hover:shadow-[0_10px_10px_#0000ff]">
+            <div className="max-w-[550px] shadow-[0_5px_10px_#0000ff] hover:shadow-[0_10px_10px_#0000ff]">
                 <img src="/album.png" alt="programming-gif" />
             </div>
         </div>
